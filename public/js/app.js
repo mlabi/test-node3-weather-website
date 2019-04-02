ff4ec5b1021cfc17d3a92866fc5fdca1c5b3fa9e
labi23 <michal.labedzki@gmail.com>
2019-04-02 21:47:30 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherFrom.addEventListener('submit', (e) => {
     messageTwo.textContent = ''
     const location = search.value
     // if (location) {
-        fetch('http://localhost:3000/weather?address='+location).then((response) => {
+        fetch('/weather?address='+location).then((response) => {
             response.json().then((data) => {
                 if (data.error) {
                     messageOne.textContent =data.error
--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 const viewPath = path.join(__dirname,'../templates/views')
 const partialsPath = path.join(__dirname,'../templates/partials')
 
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'hbs')
 app.set('views', viewPath)
 hbs.registerPartials(partialsPath)
@@ -85,6 +87,6 @@ app.get('*',(req, res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port 3000.')
 })
